7a225644c86d6914a320c8e5c3c510543a8d59d7
Sagar Poudel <poudels14@gmail.com>
2020-07-01 22:06:31 -0400

    accept uploading only base64 encoded content
      - some other changes as well

--- staticserver/src/server.go
+++ staticserver/src/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"os"
@@ -12,16 +13,25 @@ import (
 )
 
 var (
-	addr = flag.String("addr", "0.0.0.0:8003", "TCP address to listen to")
-	dir  = flag.String("dir", "/data/newsletters/", "Directory to store/serve newsletters from")
+	host = flag.String("host", "0.0.0.0:8003", "TCP address to listen to")
+	dir  = flag.String("dir", "/data/static/", "Directory to store/serve static files from from")
 )
 
+func saveFile(bucket *blob.Bucket, filename string, content *[]byte) string {
+	err := bucket.WriteAll(context.Background(), filename, *content, nil)
+	if err != nil {
+		panic("Error writing to file")
+	}
+	return filename
+}
+
 func main() {
-	fmt.Println("Running newsletter server...")
-	fmt.Println("Serving files from:", dir)
+	flag.Parse()
+	fmt.Println("Running static server...")
+	fmt.Println("Serving files from:", *dir)
 
 	if err := os.MkdirAll(*dir, 0777); err != nil {
-		panic("Couldn't create newsletters upload directory")
+		panic("Couldn't create static files upload directory")
 	}
 	bucket, err := fileblob.OpenBucket(*dir, nil)
 	if err != nil {
@@ -31,14 +41,18 @@ func main() {
 
 	app := fiber.New()
 
-	// Match any route
+	// Sample middleware
 	app.Use(func(c *fiber.Ctx) {
-		// fmt.Println("First middleware")
 		c.Next()
 	})
 
+	app.Get("/ok", func(c *fiber.Ctx) {
+		c.Send("OK")
+	})
+
 	app.Get("/newsletters/delete/*", func(c *fiber.Ctx) {
-		files := bucket.List(&blob.ListOptions{Prefix: c.Params("*")})
+		directoryToDelete := fmt.Sprintf("newsletters/%s", c.Params("*"))
+		files := bucket.List(&blob.ListOptions{Prefix: directoryToDelete})
 
 		ctxt := context.Background()
 		for {
@@ -61,18 +75,39 @@ func main() {
 		request := new(Request)
 		c.BodyParser(request)
 
-		err := bucket.WriteAll(context.Background(), request.RelativePath, []byte(request.Content), nil)
-		if err != nil {
-			panic("Error writing to file")
-		}
-		c.JSON(Response{URI: request.RelativePath})
+		b := []byte(request.Content)
+		filename := saveFile(bucket, fmt.Sprintf("newsletters/%s", request.RelativePath), &b)
+		c.JSON(Response{URI: filename})
 	})
 
-	app.Static("/", *dir, fiber.Static{
+	app.Post("/upload", func(c *fiber.Ctx) {
+		type Request struct {
+			RelativePath  string `json:"relativePath"`
+			Base64Content string `json:"base64Content"`
+		}
+		type Response struct {
+			URI   string `json:"uri"`
+			Error string `json:"error"`
+		}
+		request := new(Request)
+		c.BodyParser(request)
+
+		if request.Base64Content == "" {
+			c.JSON(Response{Error: "Missing base64Content in the body JSON"})
+			return
+		}
+
+		content, _ := base64.StdEncoding.DecodeString(request.Base64Content)
+		filename := saveFile(bucket, request.RelativePath, &content)
+		fmt.Println(fmt.Sprintf("File added: %s", filename))
+		c.JSON(Response{URI: filename})
+	})
+
+	app.Static("/", "/data/", fiber.Static{
 		Compress:  true,
 		ByteRange: true,
 		Browse:    true,
 	})
 
-	app.Listen(*addr)
+	app.Listen(*host)
 }
