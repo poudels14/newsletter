530479c4cdea0d108abe4be08356096e5921ecfd
Sagar Poudel <poudels14@gmail.com>
2020-07-28 16:35:16 -0400

    added Subash dai's newsletter filters

--- schemas/populate_gmail_newsletter_filters.sql
+++ schemas/populate_gmail_newsletter_filters.sql
@@ -2,4 +2,15 @@ INSERT INTO gmail_newsletter_filters(filter)
 VALUES
 ('from:substack.com'),
 ('from:stratechery.com'),
-('from:dailydigest@atom.finance');
+('from:dailydigest@atom.finance'),
+('from:morningbrew.com'),
+-- subash dai's filters
+('from:noreply@mail.bloombergview.com'),
+('from:rangewidely@gmail.com'),
+('from:nick@ritholtzwealth.com'),
+('from:kris@moontowermeta.com'),
+('from:annelaure@nesslabs.com'),
+('from:contact@moretothat.com')
+-- end of sbash dai's filters
+
+ON DUPLICATE KEY update filter=filter;
