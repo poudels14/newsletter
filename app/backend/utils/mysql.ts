88c5e51a9ed464656a74bd74c03b80f2cd114350
Sagar Poudel <poudels14@gmail.com>
2020-09-07 21:48:43 -0600

    fix issues

--- app/backend/utils/mysql.ts
+++ app/backend/utils/mysql.ts
@@ -7,7 +7,7 @@ const pool = createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'alpinereader',
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -28,7 +28,7 @@ const knex = Knex({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'alpinereader',
+    database: process.env.MYSQL_DATABASE,
   },
 });
 
