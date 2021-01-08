cd7d0dffa2ff711fca79c7e960de515300379f68
Sagar <poudels14@gmail.com>
2021-01-08 17:37:49 -0600

    dont count digest with originalContent = NULL
      - as well as is_newsletter = false

--- app/backend/api/newsletters/listnewsletters.ts
+++ app/backend/api/newsletters/listnewsletters.ts
@@ -7,7 +7,7 @@ const queryNewsletters = async ({ userId }: Record<string, string>) => {
     `SELECT n.id, n.name, n.authorName, n.authorEmail, COUNT(ue.unread) as totalDigests, CAST(SUM(ue.unread) AS UNSIGNED) as totalUnread
      FROM newsletters n
      LEFT JOIN user_emails ue ON ue.newsletter_id = n.id
-     WHERE n.visible = 1 AND ue.user_id IN (?)
+     WHERE n.visible = 1 AND ue.originalContent IS NOT NULL AND ue.is_newsletter = 1 AND ue.user_id IN (?)
      GROUP BY n.id`,
     [userId]
   );
